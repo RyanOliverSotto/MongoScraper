25048fbf5241edbd1e8d9a9b5583c63549bd0afd
ROSotto <RyanOSotto@Gmail.com>
2018-07-26 16:00:37 -0500

    Troubleshooting mongolab

--- server.js
+++ server.js
@@ -41,8 +41,17 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
+/*
+RyanS@DESKTOP-3UC3TB9 MINGW64 ~/iCloudDrive/nu.homework/week18/MongoScraper (master)
+$ heroku addons:create mongolab
+Creating mongolab on mongoscrapernyt... free
+Welcome to mLab.  Your new subscription is being created and will be available shortly.  Please consult the mLab Add-on Admin UI to check on its progress.
+Created mongolab-transparent-52704 as MONGODB_URI
+Use heroku addons:docs mongolab to view documentation
+*/
+mongoose.connect("mongodb://rosotto:J0sh85Ni@ds153851.mlab.com:53851/nytimesscraped", { useMongoClient: true });
 //mongoose.connect("mongodb://heroku_jmv816f9:5j1nd4taq42hi29bfm5hobeujd@ds133192.mlab.com:33192/heroku_jmv816f9");
-mongoose.connect("mongodb://localhost:27017/nytimesmongoosescrape", { useMongoClient: true });
+//mongoose.connect("mongodb://localhost:27017/nytimesmongoosescrape", { useMongoClient: true });
 
 var db = mongoose.connection;
 
